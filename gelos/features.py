3083b11ae21f764ceee557e83151dbf85c1e2c5a
Denys Godwin <denyswgodwin@gmail.com>
2025-11-26 01:36:58 -0500

    changes for full visualizations, migrate embedding generation into module

--- gelos/features.py
+++ gelos/features.py
@@ -1,29 +1,51 @@
 from pathlib import Path
-
+import torch
+from gelos.gelosdatamodule import GELOSDataModule
+import yaml
+from gelos import config
+from lightning.pytorch import Trainer
+from pathlib import Path
 from loguru import logger
 from tqdm import tqdm
 import typer
 
-from gelos.config import PROCESSED_DATA_DIR
-
 app = typer.Typer()
+from gelos.config import PROJ_ROOT, PROCESSED_DATA_DIR, DATA_VERSION, RAW_DATA_DIR
+from terratorch.tasks import EmbeddingGenerationTask
+class LenientEmbeddingGenerationTask(EmbeddingGenerationTask):
+    def check_file_ids(self, file_ids, x):
+        return
 
+def generate_embeddings(yaml_path: Path) -> None:
+
+    with open(yaml_path, "r") as f:
+        yaml_config = yaml.safe_load(f)
+    
+    print(yaml.dump(yaml_config))
+
+    model_name = yaml_config['model']['init_args']['model']
+    output_dir = PROCESSED_DATA_DIR / DATA_VERSION / model_name
+    output_dir.mkdir(exist_ok=True, parents=True)
+    data_root = RAW_DATA_DIR / DATA_VERSION
+
+    # add variables to yaml config so it can be passed to classes
+    yaml_config['data']['init_args']['data_root'] = data_root
+    yaml_config['model']['init_args']['output_dir'] = output_dir
+
+    gelos_datamodule = GELOSDataModule(**yaml_config['data']['init_args'])
+    task = LenientEmbeddingGenerationTask(**yaml_config['model']['init_args'])
+
+    device = 'gpu' if torch.cuda.is_available() else 'cpu'
+    trainer = Trainer(accelerator=device, devices=1)
+
+    trainer.predict(model=task, datamodule=gelos_datamodule)
 
 @app.command()
-def main(
-    # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    input_path: Path = PROCESSED_DATA_DIR / "dataset.csv",
-    output_path: Path = PROCESSED_DATA_DIR / "features.csv",
-    # -----------------------------------------
-):
-    # ---- REPLACE THIS WITH YOUR OWN CODE ----
-    logger.info("Generating features from dataset...")
-    for i in tqdm(range(10), total=10):
-        if i == 5:
-            logger.info("Something happened for iteration 5.")
-    logger.success("Features generation complete.")
-    # -----------------------------------------
-
+def main():
+    yaml_config_directory = PROJ_ROOT / 'gelos' / 'configs'
+    yaml_paths = list(yaml_config_directory.glob('*.yaml'))
+    for yaml_path in yaml_paths:
+        generate_embeddings(yaml_path)
 
 if __name__ == "__main__":
     app()
